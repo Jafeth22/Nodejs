e6f1f12e83798d6d0418902c7222d0fb2684b67c
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2021-08-19 10:54:53 -0600

    Understanding routes from server

--- 2-Protocole HTTP and Web Service/idx.js
+++ 2-Protocole HTTP and Web Service/idx.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 const html = fs.readFileSync('./index.html')
+const about = fs.readFileSync('./about.html')
 
 /**
  * request = From client to server
@@ -9,8 +10,16 @@ const html = fs.readFileSync('./index.html')
  */
 http
     .createServer((request, response) => {
+        const {url} = request
         response.writeHead(200, {"Content-Type": "text/html"})
-        response.write(html);
+        if (url === '/') {
+            response.write(html);
+        }else if(url === '/about') {
+            response.write(about);
+        }else{
+            response.writeHead(404, {"Content-Type": "text/html"})
+            response.write("Page not found");
+        }
         response.end(); //The task has been completed
     })
     .listen(8080); //The servers should send the responder through some port, so, here we define it
