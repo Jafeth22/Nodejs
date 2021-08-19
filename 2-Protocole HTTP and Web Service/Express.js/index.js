f4da68a6c29f7f7b8e469298de1ad25ea645ab50
Jafeth VV <33306997+Jafeth22@users.noreply.github.com>
2021-08-19 11:47:22 -0600

    Add routes

--- 2-Protocole HTTP and Web Service/Express.js/index.js
+++ 2-Protocole HTTP and Web Service/Express.js/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const server = express();
 
+const fs = require('fs');
+const home = fs.readFileSync('./index.html')
+const about = fs.readFileSync('./about.html')
+
+server.get('/', function (req, res) {
+    res.write(home)
+})
+
+server.get('/about', function (req, res) {
+    res.write(about)
+})
+
 server.listen(8080, () => {
     console.log("Server is running on port 8080");
 })
